d6521f803f9e11c14258837e0b368735cd6683b4
WandaSimeoli <wandasimeoli@gmail.com>
2023-09-04 17:02:00 +0200

    Creation class

--- index.php
+++ index.php
@@ -15,4 +15,19 @@ class Product {
         $this->typology = $typology;
     }
 }
+class Game extends Product {
 
+}
+class Food extends Product {
+
+}
+class Bed extends Product {
+
+}
+
+$ball = new Game('https://www.shoppypet.com/5455-large_default/ferplast-pa-6060-pallina-per-cani-in-vinile.jpg',
+'Ball', '22$', 'Dog', 'game');
+// $biscuits = new Food();
+// $dogHouse = new bed();
+
+var_dump($ball);
